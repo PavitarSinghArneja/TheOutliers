195c67f815bd37f41c8f26244470a87f285e1db2
Isha Chandravanshi <ishachandravanshi06@gmail.com>
2025-08-07 03:13:14 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/TheOutliers/', // 👈 Base path for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': '/src', // 👈 Simple path alias without Node's path module
     },
   },
   optimizeDeps: {
